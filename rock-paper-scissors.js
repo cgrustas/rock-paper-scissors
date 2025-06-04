dbed97385d405b84bf166e43af037ced261873dd
Coleman Grustas <chgrustas@gmail.com>
2025-06-04 13:07:26 -0400

    Define getHumanChoice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,13 @@
 function getComputerChoice() {
     let x = Math.random();
-    // console.log("Random value x:", x);
+    console.log("Random value x:", x);
     if (x < 1/3) return "rock";
     else if (x < 2/3) return "paper";
     else return "scissors";
+}
+
+function getHumanChoice() {
+    let choice = prompt("rock, paper, scissors?");
+    console.log(choice);
+    return choice;
 }
