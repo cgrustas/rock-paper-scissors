001930dc6ec988027057649c82519372c7520223
Coleman Grustas <chgrustas@gmail.com>
2025-06-04 13:27:22 -0400

    Write the logic to play a single round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,13 +1,67 @@
-
 let humanScore = 0;
 let computerScore = 0;
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLowerCase();
+    let answer;
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            answer = "Try again! You both chose rock";
+        }
+        else if (computerChoice == "paper") {
+            answer = "You lose! Paper beats rock";
+            computerScore++;
+        }
+        else {
+            answer = "You win! Rock beats scissors";
+            humanScore++;
+        }
+    }
+    else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            answer = "You win! Paper beats rock";
+            humanScore++;
+        }
+        else if (computerChoice == "paper") {
+            answer = "Try again! You both chose paper";
+        }
+        else {
+            answer = "You lose! Scissors beats paper";
+            computerScore++;
+        }
+    }
+    else {
+        if (computerChoice == "rock") {
+            answer = "You lose! Rock beats scissors";
+            computerScore++;
+        }
+        else if (computerChoice == "paper") {
+            answer = "You win! Scissors beats paper";
+            humanScore++;
+        }
+        else {
+            answer = "Try again! You both chose scissors";
+        }
+    }
+    console.log(answer);
+    console.log("Human score: ", humanScore);
+    console.log("Computer score: ", computerScore);
+    return answer;
+}
+
 function getComputerChoice() {
     let x = Math.random();
-    console.log("Random value x:", x);
-    if (x < 1/3) return "rock";
-    else if (x < 2/3) return "paper";
-    else return "scissors";
+    let result;
+    if (x < 1/3) result = "rock";
+    else if (x < 2/3) result = "paper";
+    else result = "scissors";
+    console.log("Computer choice: ", result);
+    return result;
 }
 
 function getHumanChoice() {
