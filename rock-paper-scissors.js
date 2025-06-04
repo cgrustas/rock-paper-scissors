23bc8c2ad342eae94202f7da8e6ce4a602f98bab
Coleman Grustas <chgrustas@gmail.com>
2025-06-04 12:59:57 -0400

    Comment out console log test

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     let x = Math.random();
-    console.log("Random value x:", x);
+    // console.log("Random value x:", x);
     if (x < 1/3) return "rock";
     else if (x < 2/3) return "paper";
     else return "scissors";
