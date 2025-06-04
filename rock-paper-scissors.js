1d7286c2d0eb4cff3fdcbdca4e68e3f49cc59dfe
Coleman Grustas <chgrustas@gmail.com>
2025-06-04 12:56:41 -0400

    Make the conditions more precise

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     let x = Math.random();
     console.log("Random value x:", x);
-    if (x < .33) return "rock";
-    else if (x < .66) return "paper";
+    if (x < 1/3) return "rock";
+    else if (x < 2/3) return "paper";
     else return "scissors";
 }
